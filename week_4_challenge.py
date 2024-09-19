d1d5d2b2f461fd47a2d0f0a9f2c27f721d380f57
Lemus <aglemus@cps.k12.il.us>
2024-09-19 12:47:34 -0500

    work

---
+++ week_4_challenge.py
@@ -0,0 +1,13 @@
+#################################Extracting Sub-Strings###################################
+# Extracting Sub-Strings Practice #1
+# Extract the first word of the following sentence using slicing, and display it on the screen:
+# "Controlling complexity is the essence of programming"
+# Hint: "Controlling" is 11 characters long.
+
+# Extracting Sub-Strings Practice #2
+# Take every third character starting from the ninth to the end of the sentence, and print the result.
+# "Never trust a computer you can't throw out a window"
+
+# Extracting Sub-Strings Practice #3
+# Reverses the position of all the characters in the following sentence and displays the result on the screen.
+# "It's great to work with computers. They don't argue, they remember everything and they don't drink your beer"
